19fde9a41b63279c809ee7438534b3700cc31cd7
dhanokarpramod777 <dhanokarpramod777@gmail.com>
2023-02-02 14:56:10 +0530

    2St Task

---
+++ src/Sprint1/Task_1.java
@@ -0,0 +1,18 @@
+package Sprint1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void M1(){
+        // Prime Program
+    System.out.println("Working");
+
+
+
+
+
+
+    }
+}
